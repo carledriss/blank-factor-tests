14a6eb0a9cd0853f7848e09a2c3b81754dd0f57c
Carledriss Zeiss <carledriss@gmail.com>
2025-04-29 16:25:06 -0400

    Start chrome in incognito mode

--- src/main/java/org/example/core/ui/Chrome.java
+++ src/main/java/org/example/core/ui/Chrome.java
@@ -3,12 +3,15 @@ package org.example.core.ui;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Chrome implements Browser {
 
     @Override
     public WebDriver init() {
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        return new ChromeDriver(options);
     }
 }
